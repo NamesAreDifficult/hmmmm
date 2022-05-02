4e793c6bb4536900d3cd60cddad9a12b50516ade
Brian McNulty <brianmcnulty321@gmail.com>
2022-05-02 17:40:57 -0400

    Added connection to server

--- Client/src/App.java
+++ Client/src/App.java
@@ -3,9 +3,21 @@
 // Brian McNulty NamesAreDifficult
 
 
+import java.io.IOException;
+import java.net.Socket;
+
+
 public class App {
-    public static int main(String[] args){
-        return 0;
+    public static void main(String[] args){
+        try {
+            run(args);
+        }catch(IOException exc){
+            System.err.println("Failed to create socket");
+        }
     }
 
+    public static int run(String [] args) throws IOException {
+        Socket sock = new Socket("127.0.0.1", 8080);
+        return 0;
+    }
 }
